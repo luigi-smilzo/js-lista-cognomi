511216adbc99fe269c5c868389e5bf6fafdf75fc
Luigi <luigismilzo@live.com>
2020-03-31 16:32:22 +0200

    Added surname position in HTML

--- js/main.js
+++ js/main.js
@@ -1,20 +1,14 @@
-// Srtarting surname list
 var surnameList = ['Bianchi', 'Rossi', 'Duzioni', 'Balsano', 'Verdi'];
-console.log(surnameList);
 
-// Ask the user for his surname
 var userSurname = prompt('Inserisci il tuo cognome');
 
 
-// Push user surname into list and print the new array items alphabetically
 surnameList.push(userSurname);
-console.log(surnameList.sort());
+surnameList.sort();
 
-// Check the new array item position and print
-console.log(surnameList.indexOf('Smilzo'));
+var position = surnameList.indexOf(userSurname);
 
 var unorderedList = document.getElementById('unorderedList');
-
 
 for (var i = 0; i < surnameList.length; i++) {
     var surname = surnameList[i];
@@ -23,3 +17,5 @@ for (var i = 0; i < surnameList.length; i++) {
     var newItem = '<li>' + surname + '</li>';
     unorderedList.innerHTML = previousItem + newItem;
 }
+
+document.getElementById('position').innerHTML = 'New surname position is: ' + (position + 1);
