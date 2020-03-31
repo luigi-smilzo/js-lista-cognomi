0f90f8b882d3bdb8c8b73bd0dfb22455249739c1
Luigi <luigismilzo@live.com>
2020-03-31 15:46:53 +0200

    Fix

--- js/main.js
+++ js/main.js
@@ -6,7 +6,9 @@ console.log(surnameList);
 var userSurname = prompt('Inserisci il tuo cognome');
 
 
-// Push user surname into list and print the nrw array items alphabetically
+// Push user surname into list and print the new array items alphabetically
 surnameList.push(userSurname);
 console.log(surnameList.sort());
+
+// Check the new array item position and print
 console.log(surnameList.indexOf('Smilzo'));
